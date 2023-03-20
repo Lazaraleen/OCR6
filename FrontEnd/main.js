a9c2e90dbdb18acab13061982e9335b06eaa8abe
Myriam JAYLE <myriamjayle@gmail.com>
2023-03-20 14:12:18 +0100

    reorganisation en fonction

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,70 +1,91 @@
 const gallery = document.querySelector(".gallery");
+let data = [];
+let categorie = new Array();
+let valSelect = new Array();
 
-fetch('http://localhost:5678/api/works')
-    .then (function(response) {return response.json();})
-    .then(function(data) {
-        console.log(data);
+async function fetchUser () {
+    try {
+        const response = await fetch('http://localhost:5678/api/works');
+        data = await response.json();
+        let valSelect = new Array ();
+        tabCat();
+        catego(); 
 
-        // Créer un tableau pour les catégories
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-        let categorie = new Array();
-        categorie[0]=0;
-        count=1;
+fetchUser();
 
-        for (let cat of data) {
-            categorie[count] = cat.category.name;
-            count++;
-        }
+// Créer un tableau pour les catégories
+function tabCat() {
+    categorie[0]=0;
+    count=1;
+    
+    for (let cat of data) {
+        categorie[count] = cat.category.name;
+        count++;
+    }
+}
 
-        // Trier le tableau catégorie
-
-        const sortUnique = categorie.sort();
-        const unique = new Set(categorie);
-        console.log(unique);
-
-        // Créer un bouton pour les catégories
-
-        const divcat = document.querySelector(".divcat");
-        const label = document.createElement("label");
-        const select = document.createElement("select");
-        select.setAttribute("id","category");
-        label.innerHTML = 'Catégorie :';
-
-        divcat.appendChild(label);
-        divcat.appendChild(select);
-
-        for (let choix of unique) {
-            const option = document.createElement("option");
-            if (choix === 0) {option.innerText = "Toutes catégories";
-                option.value = 'Toutes catégories';}
-                else { option.innerText = choix;
-                    option.value = choix;};
-            select.appendChild(option);
-        }
-
-        // Faire le tri suivant la catégorie choisie
-
-
-
-
+// Afficher les projets pour les retirer du HTML
+function project(key){
+    for (let i of data) {
+        const figureElement = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        const figcaptionElement = document.createElement("figcaption");
+        imageElement.src = i.imageUrl;
+        imageElement.alt = i.title;
+        figcaptionElement.innerText = i.title;
+    
+        gallery.appendChild(figureElement);
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(figcaptionElement);
+    }
+}
 
 
-        // Afficher les projets pour les retirer du HTML
+// Créer un bouton pour les catégories
+function catego(){
+    const divcat = document.querySelector(".divcat");
+    const label = document.createElement("label");
+    const select = document.createElement("select");
+    select.setAttribute("id","category");
+    select.setAttribute("onchange", "getSelectedValue()");
+    label.innerHTML = 'Catégorie :';
+    
+    // Trier le tableau catégorie
+    const sortUnique = categorie.sort();
+    const unique = new Set(categorie);
+    
+    divcat.appendChild(label);
+    divcat.appendChild(select);
+    
+    for (let choix of unique) {
+        const option = document.createElement("option");
+        if (choix === 0) {option.innerText = "Toutes catégories";
+            option.value = 'Toutes catégories';}
+            else { option.innerText = choix;
+                option.value = choix;};
+        select.appendChild(option);
+    }
 
-        for (let i of data) {
-            const figureElement = document.createElement("figure");
-            const imageElement = document.createElement("img");
-            const figcaptionElement = document.createElement("figcaption");
-            imageElement.src = i.imageUrl;
-            imageElement.alt = i.title;
-            figcaptionElement.innerText = i.title;
+    filter(valSelect);
+}
 
-            gallery.appendChild(figureElement);
-            figureElement.appendChild(imageElement);
-            figureElement.appendChild(figcaptionElement);
-        }
+function getSelectedValue ()
+{
+    let selectedValue = document.getElementById("category").value;
+    // console.log(selectedValue);
+    valSelect = selectedValue;
+    console.log(valSelect);
+}
 
-    });
-
-
+// Filtrer les produits
+function filter (value) {
+    if (value !== 'Toutes catégories') {project(value);}
+    else {project(data);};
+}
 
